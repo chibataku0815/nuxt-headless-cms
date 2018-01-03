aee4d6f0525b8f86de6e11d4517a2e1254889093
千葉工 <chibataku0815@gmail.com>
2018-01-03 17:19:11 +0900

    axios test01

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,7 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  plugins: ['~/plugins/axios'],
   /*
   ** Customize the progress bar color
   */
